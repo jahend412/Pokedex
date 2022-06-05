1e9dec50e6c42acfe851b1f8f4a21a67a64248e0
Josh Endemann <jahend44@gmail.com>
2022-06-05 11:39:28 -0500

    Added to scripts.js and fixed index.
    Added a lang to html tag in index.  Also changed the title to Pokedex.
    Added weight to the array in scripts.js.
    Also added the loop.

--- simple-js-app/js/scripts.js
+++ simple-js-app/js/scripts.js
@@ -1,7 +1,20 @@
 let pokemonList = [
-    {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
-    {name: 'Charmander', height: 0.6, types: 'fire'},
-    {name: 'Squirtle', height: 0.5, types: 'water'},
-    {name: 'Pikachu', height: 0.4, types: 'Electric'},
-    {name: 'Mew', height: 0.4, types: 'Psychic'}
+    {name: 'Bulbasaur', height: 0.7, weight: 6.9, types: ['grass', 'poison']},
+    {name: 'Charmander', height: 0.6, weight: 8.5, types: 'fire'},
+    {name: 'Squirtle', height: 0.5, weight: 9, types: 'water'},
+    {name: 'Pikachu', height: 0.4, weight: 6, types: 'Electric'},
+    {name: 'Mewtwo', height: 2.0, weight: 122, types: 'Psychic'}
 ];
+
+for (var i = 0; i < pokemonList.length; i++) {
+  document.write(pokemonList[i].name + " height is "+ pokemonList[i].height + " m!");
+
+    if(pokemonList[i].height > 1.7) {
+      document.write("  This pokemon is tall.  Wow, that\'s big!" + "<br>");
+    }else if(pokemonList[i].height > 1.0  && pokemonList[i].height < 1.7) {
+      document.write("  This pokemon has an average height!" + "<br>");
+    }else {
+      document.write("  This pokemon is short!" + "<br>");
+    }
+
+}

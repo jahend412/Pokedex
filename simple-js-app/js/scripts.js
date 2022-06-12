68a6e36dc9d9431cb5ca229c8837a068830a4f04
Josh Endemann <jahend44@gmail.com>
2022-06-12 06:22:13 -0500

    Added IIFE function and forEach function
    Made small 'clean up' fixes to the types in the pokemonList by using capital letters.
    
    Added Onix to the repository

--- simple-js-app/js/scripts.js
+++ simple-js-app/js/scripts.js
@@ -1,20 +1,39 @@
-let pokemonList = [
-    {name: 'Bulbasaur', height: 0.7, weight: 6.9, types: ['grass', 'poison']},
-    {name: 'Charmander', height: 0.6, weight: 8.5, types: 'fire'},
-    {name: 'Squirtle', height: 0.5, weight: 9, types: 'water'},
+
+let pokemonRepository = (function () {  //  Entered the IIFE function and now the pokemonList is "protected"
+  let pokemonList = [
+    {name: 'Bulbasaur', height: 0.7, weight: 6.9, types: ['Grass', 'Poison']},
+    {name: 'Charmander', height: 0.6, weight: 8.5, types: 'Fire'},
+    {name: 'Squirtle', height: 0.5, weight: 9, types: 'Water'},
     {name: 'Pikachu', height: 0.4, weight: 6, types: 'Electric'},
     {name: 'Mewtwo', height: 2.0, weight: 122, types: 'Psychic'}
-];
-
-for (var i = 0; i < pokemonList.length; i++) {
-  document.write(pokemonList[i].name + " height is "+ pokemonList[i].height + " m!");
-
-    if(pokemonList[i].height > 1.7) {
-      document.write("  This pokemon is tall.  Wow, that\'s big!" + "<br>");
-    }else if(pokemonList[i].height > 1.0  && pokemonList[i].height < 1.7) {
-      document.write("  This pokemon has an average height!" + "<br>");
-    }else {
-      document.write("  This pokemon is short!" + "<br>");
-    }
-
+  ];
+function add(pokemon) {
+  pokemonList.push(pokemon);
 }
+
+function getAll() {    //Added getAll function
+  return pokemonList;
+}
+
+return {
+  add: add,
+  getAll: getAll    //Calling getAll function
+};
+})();
+
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.add({ name: 'Onix', height: 8.8, weight: 210, types: 'Rock', 'Ground'});  //Added Onix to repository
+
+console.log(pokemonRepository.getAll());
+
+pokemonList.forEach(function(pokemon) {  // Added forEach loop
+  document.write(pokemon.name, '(height: ' + pokemon.height +')');
+    if(pokemonList[i].height > 1.7) {
+    document.write("  This pokemon is tall.  Wow, that\'s big!" + "<br>");
+    }else if(pokemonList[i].height > 1.0  && pokemonList[i].height < 1.7) {
+    document.write("  This pokemon has an average height!" + "<br>");
+    }else {
+    document.write("  This pokemon is short!" + "<br>");
+  }
+});

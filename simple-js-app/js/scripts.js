693dc136c8dbdf8fd4af847edbd6f644869959b7
Josh Endemann <jahend44@gmail.com>
2022-06-15 04:33:31 -0500

    fixed syntax error.  Entered square brackets on types:

--- simple-js-app/js/scripts.js
+++ simple-js-app/js/scripts.js
@@ -2,10 +2,10 @@
 let pokemonRepository = (function () {  //  Entered the IIFE function and now the pokemonList is "protected"
   let pokemonList = [
     {name: 'Bulbasaur', height: 0.7, weight: 6.9, types: ['Grass', 'Poison']},
-    {name: 'Charmander', height: 0.6, weight: 8.5, types: 'Fire'},
-    {name: 'Squirtle', height: 0.5, weight: 9, types: 'Water'},
-    {name: 'Pikachu', height: 0.4, weight: 6, types: 'Electric'},
-    {name: 'Mewtwo', height: 2.0, weight: 122, types: 'Psychic'}
+    {name: 'Charmander', height: 0.6, weight: 8.5, types: ['Fire']},
+    {name: 'Squirtle', height: 0.5, weight: 9, types: ['Water']},
+    {name: 'Pikachu', height: 0.4, weight: 6, types: ['Electric']},
+    {name: 'Mewtwo', height: 2.0, weight: 122, types: ['Psychic']}
   ];
 
 function add(pokemon) {   //Entered add function that declares typeof pokemon
@@ -18,11 +18,11 @@ function getAll() {    //Added getAll function to get all pokemon
 
 function addListItem(pokemon) {  // Function addListItem is used for DOM
   let pokemonList = document.querySelector(".pokemon-list");  //.pokemon-list is ul in index
-  let listpokemon = document.createElement("li");   // Creating listpokemon as a list
+  let listPokemon = document.createElement("li");   // Creating listpokemon as a list
   let button = document.createElement("button");  // creating a button
   button.innerText = pokemon.name;   // creating text to be pokemon names for button
   button.classList.add("button-class");  // Having the button take on style from css
-  listpokemon.appendchild(button);  // calling the listpokemon to the button
+  listPokemon.appendchild(button);  // calling the listpokemon to the button
   pokemonList.appendChild(listpokemon);  // calling the pokemonList to the list
   eventListener(button, pokemon);  //  added eventListener with two parameters
 }
@@ -48,7 +48,7 @@ return {
 
 console.log(pokemonRepository.getAll());
 
-pokemonRepository.add({ name: 'Onix', height: 8.8, weight: 210, types: 'Rock', 'Ground'});  //Added Onix to repository
+pokemonRepository.add({ name: 'Onix', height: 8.8, weight: 210, types: ['Rock', 'Ground']});  //Added Onix to repository
 
 console.log(pokemonRepository.getAll());
 
